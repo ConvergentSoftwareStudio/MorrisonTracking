deafe0fb15204126bb2bdb7019cd4cb9334983b6
tgulstine <timothy.gulstine@gmail.com>
2015-03-10 17:56:47 -0500

    Tracking page with new fake shipment data

---
+++ shipments.js
@@ -0,0 +1,23 @@
+var shipments = [
+{
+shipper: "Shanghai Auto Parts Mfg, No. 348, Yuanqu Road, Anting Town, Jiading Dist., Shanghai, China (Mainland)",
+consignee: "ABC Co., Los Angeles, CA", AWB: "089-6934265-5", shipmentDate: "02 / 04 / 2015", arrivalDate: "02 / 12 / 2015"
+},
+{
+shipper: "Happy Trading CA, 27 Shaughnesey St., Vancouver, BC, Canada",
+consignee: "Happy Trading USA, 200 West Grand Ave., Woodstock, WI", AWB: "014-3218614-4", shipmentDate: "02 / 05 / 2015", arrivalDate: "02 / 09 / 2015"
+},
+{
+shipper: "Ideal Systems, 2/F, Tower Two, 88 Container Port Road, HK",
+consignee: "Apple, 1 Infinite Loop, Cupertino, CA", AWB: "043-2139573-4", shipmentDate: "02 / 07 / 2015", arrivalDate: "02 / 13 / 2015"
+},
+{
+shipper: "IO Supply Company, 63 Renmin Lu, Qingdao Shi, 266033 Shandong, China",
+consignee: "McDonald's, 2111 McDonald's Dr., Oak Brook, IL 60523", AWB: "089-0347293-4", shipmentDate: "02 / 13 / 2015", arrivalDate: "02 / 20 / 2015"
+},
+{
+shipper: "ABI Global, Inc., Urion 30, Puerto Juarez, 02860 Mexico, D.F., Mexico",
+consignee: "Amazon Com, 410 Terry Avenue North, Seattle, WA 98109", AWB: "078-3751392-4", shipmentDate: "02 / 13 / 2015", arrivalDate: "02 / 18 / 2015"
+}];
+
+// define the columns for the two grids
