9bcb4f79488ec936ec7e6f6dd86c4c7598028db8
tgulstine <timothy.gulstine@gmail.com>
2015-03-16 07:48:24 -0500

    Added recent deliveries mock-up

--- MorrisonDashboard/MorrisonDashboard/Scripts/InTransitCtrl.js
+++ MorrisonDashboard/MorrisonDashboard/Scripts/InTransitCtrl.js
@@ -1,5 +1,6 @@
 ﻿morrisonApp.controller("InTransitCtrl", function ($scope) {
     $scope.InTransitData = inTransitData;
+    $scope.MilestoneData = milestoneData;
     $scope.InTransitColumnDefinitions = [{ field: 'AWB', displayName: 'AWB' }, { field: 'shipper', displayName: 'Shipper' },
                                 { field: 'consignee', displayName: 'Consignee' }];
 
@@ -9,12 +10,33 @@
         data: 'InTransitData',
         columnDefs: 'InTransitColumnDefinitions',
         selectedItems: [],
-        multiSelect: false // ,
-        //rowTemplate: '<div ng-style="{ \'background-color\' : getColorCoding(row.entity) }" >' +
-        //                            '<div ng-style="{ \'cursor\': row.cursor }" ng-repeat="col in renderedColumns" ng-class="col.colIndex()" class="ngCell {{col.cellClass}}">' +
-        //                                '<div class="ngVerticalBar" ng-style="{height: rowHeight}" ng-class="{ ngVerticalBarVisible: !$last }">&nbsp;</div>' +
-        //                                '<div ng-cell></div>' +
-        //                            '</div>' +
-        //                      '</div>'
+        multiSelect: false,
+        rowTemplate: '<div ng-style="{ \'background-color\' : getColorCoding(row.entity) }" >' +
+                                    '<div ng-style="{ \'cursor\': row.cursor }" ng-repeat="col in renderedColumns" ng-class="col.colIndex()" class="ngCell {{col.cellClass}}">' +
+                                        '<div class="ngVerticalBar" ng-style="{height: rowHeight}" ng-class="{ ngVerticalBarVisible: !$last }">&nbsp;</div>' +
+                                        '<div ng-cell></div>' +
+                                    '</div>' +
+                              '</div>'
+    }
+
+    $scope.getColorCoding = function (row) {
+        var dateDue, dateCompleted, color = "green";
+
+        for (var i = 0; i < $scope.MilestoneData.length; i++) {
+            if ($scope.MilestoneData[i].AWB == row.AWB) {
+                dateDue = new Date($scope.MilestoneData[i].DateDue);
+                if ($scope.MilestoneData[i].DateCompleted == "") {
+                    if (dateDue < currentDate) {
+                        color = "red";
+                    } else {
+                        if (dateDue.getTime() === currentDate.getTime()) {
+                            color = "yellow";
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        return color;
     }
 });
