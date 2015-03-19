0bfa36650f9290d7568c895a952e485e7b359a5d
tgulstine <timothy.gulstine@gmail.com>
2015-03-18 21:49:27 -0500

    added more content

--- MorrisonDashboard/MorrisonDashboard/Scripts/ShipmentGridCtrl.js
+++ MorrisonDashboard/MorrisonDashboard/Scripts/ShipmentGridCtrl.js
@@ -1,15 +1,15 @@
-﻿morrisonApp.controller("ShipmentGridCtrl", function ($scope) {
+﻿morrisonApp.controller("ShipmentGridCtrl", function ($scope, ngDialog) {
     $scope.ShipmentGridData = shipmentGridData;
-    $scope.ShipmentGridColumnData = [      { field: 'AWB', displayName: 'AWB' },
+    $scope.ShipmentGridColumnData = [{ field: 'AWB', displayName: 'AWB', width: 100, cellTemplate: '<div ng-click="openShipmentDetails(row.entity[col.field])" class="link">{{row.entity[col.field]}}</div>' },
                                                                         { field: 'shipDate', displayName: 'Ship Date' },
                                                                         { field: 'dueDate', displayName: 'Due Date' },
                                                                         { field: 'origin', displayName: 'Origin' },
                                                                         { field: 'ORG', displayName: 'ORG' },
                                                                         { field: 'destination', displayName: 'Destination' },
                                                                         { field: 'DST', displayName: 'DST' },
-                                                                        { field: 'pcs', displayName: 'Pcs' },
-                                                                        { field: 'wgt', displayName: 'Wgt' },
-                                                                        { field: 'status', displayName: 'Status' }
+                                                                        { field: 'pcs', displayName: 'Pcs', width: 40 },
+                                                                        { field: 'wgt', displayName: 'Wgt', width: 40 },
+                                                                        { field: 'status', displayName: 'Status', width: 40 }
     ];
 
     // Shipments is the data for the grid (in quotes, which is weird, but that's how AngularJS wants it)
@@ -19,6 +19,14 @@
         columnDefs: 'ShipmentGridColumnData',
         selectedItems: [],
         multiSelect: false
+    }
+
+    $scope.openShipmentDetails = function (awb) {
+        console.log(awb);
+        ngDialog.open({
+            template: 'shipmentDetailTabs.html',
+            width: 800
+        });
     }
 
 });
